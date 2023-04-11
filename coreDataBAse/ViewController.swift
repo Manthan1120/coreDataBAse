0b190dfce924702533399bf0512fa5c4ed0d1dce
Manthan Kevadiya <manthankevadiya11@gmail.com>
2023-04-11 11:38:00 +0530

    354

--- coreDataBAse/ViewController.swift
+++ coreDataBAse/ViewController.swift
@@ -1,19 +1,88 @@
-//
-//  ViewController.swift
-//  coreDataBAse
-//
-//  Created by R86 on 06/04/23.
-//
-
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextFeild: UITextField!
+    @IBOutlet weak var idTextFeild: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func saveButtonAction(_ sender: Any) {
+        if let x = idTextFeild.text, let y = Int(x){
+            addData(name: nameTextFeild.text!, id: y)
+        }
+    }
+    
+    @IBAction func deleteButtonAction(_ sender: Any) {
+        if let x = idTextFeild.text, let y = Int(x){
+            deleteData(id: y)
+        }
+    }
+    @IBAction func showButtonAction(_ sender: Any) {
+        getData()
+    } 
+    
+    func addData(name: String,id:Int) {
+        
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContext = appDeleget.persistentContainer.viewContext
+        
+        let userEntity = NSEntityDescription.entity(forEntityName: "Students", in: manageContext)
+        let user = NSManagedObject(entity: userEntity!, insertInto: manageContext)
+        user.setValue(name, forKey: "name")
+        user.setValue(id, forKey: "id")
+        print(user)
+    }
+    
+    func updateData() {
+        
+    }
+    
+    func getData() {
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContext = appDeleget.persistentContainer.viewContext
+        
+        let fetchRequest = Students.fetchRequest()
+        
+        do{
+            let result = try manageContext.fetch(fetchRequest)
+            for data in result {
+                print(data.name as! String,data.id)
+            }
+        }
+        catch {
+            print("Could not save.")
+        }
+    
+    }
+    
+    func deleteData(id:Int) {
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContext = appDeleget.persistentContainer.viewContext
+        let fatchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Students")
+        fatchRequest.predicate = NSPredicate(format: "id = %d", id)
+        
+        do{
+            let test = try manageContext.fetch(fatchRequest)
+            let objToDelete = test[0] as! NSManagedObject
+            manageContext.delete(objToDelete)
+            appDeleget.saveContext()
+            print("Data delete")
+        }
+        catch{
+            print(error)
+        }
+        
+    }
+    
+    func navigate() {
+       
+        let navigation = storyboard?.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
+        navigationController?.pushViewController(navigation, animated: true)
+    }
+    
 }
 
